cadaf7e4a3a00fefe0318c1b099e31a3b935ef80
DominicObil <145765375+DominicObil@users.noreply.github.com>
2023-10-06 13:42:54 +0200

    Create dias_semana.java

---
+++ dias_semana.java
@@ -0,0 +1,39 @@
+	
+		int numero;
+		
+		
+
+
+
+		Scanner teclado = new Scanner(System.in);//
+		System.out.printf("introduzca numero");
+		numero =teclado.nextInt();
+
+
+		 if(numero==1)
+			 System.out.printf("el dia es lunes");
+		 if(numero==2)
+			 System.out.printf("el dia es martes");
+		 if(numero==3)
+			 System.out.printf("el dia es miercoles");
+				
+		 if(numero==4)
+			 System.out.printf("el dia es jueves");
+				
+		 if(numero==5)
+			 System.out.printf("el dia es viernes");
+				
+		 if(numero==6)
+			 System.out.printf("el dia es sabado");
+				
+		 if(numero==7)
+			 System.out.printf("el dia es domingo");
+		 else 
+			 System.out.printf("ERROR");
+				
+				
+			
+		    
+		 
+	}
+	}
